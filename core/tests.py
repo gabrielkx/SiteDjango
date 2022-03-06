45e25fef1c04e24e44679a9e9bff782a1bbe573e
gabriel <gabrielgamersbr@gmail.com>
2022-03-06 16:40:56 -0300

    testes na home

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class Hometest(TestCase):
+    def setUp(self):
+        self.response =self.client.get('/')
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+         #must use index.html
+        self.assertTemplateUsed(self.response, 'index.html')

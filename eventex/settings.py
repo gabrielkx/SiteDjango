a72ae37faf2f00baf043a9e8d1f51e8219798f47
gabriel <gabrielgamersbr@gmail.com>
2022-03-08 20:49:58 -0300

    testes

--- eventex/settings.py
+++ eventex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-DEFAULT_FROM_EMAIL = 'gabrielgamersbr@gmail.com'
+DEFAULT_FROM_EMAIL = 'seucarlinhos6@gmail.com'
 
 
 # Application definition

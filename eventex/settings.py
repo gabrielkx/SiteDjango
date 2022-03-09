c713a671b8e076ce6743d7afcce96f132a8c8c2e
gabriel <gabrielgamersbr@gmail.com>
2022-03-08 21:33:21 -0300

    teste

--- eventex/settings.py
+++ eventex/settings.py
@@ -22,15 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+#DEBUG = config('DEBUG', default=False, cast=bool)
 
-#SECRET_KEY='django-insecure-3a2c^7*sr#vr5t_8jucb2jwtinyqu7k=ggi#mlf(#fvjibj81+'
-#DEBUG='True'
-#ALLOWED_HOSTS='127.0.0.1, localhost, .herokuapp'
+SECRET_KEY='django-insecure-3a2c^7*sr#vr5t_8jucb2jwtinyqu7k=ggi#mlf(#fvjibj81+'
+DEBUG='True'
+ALLOWED_HOSTS='127.0.0.1, localhost, .herokuapp'
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 DEFAULT_FROM_EMAIL = 'seucarlinhos6@gmail.com'
 
 
@@ -142,3 +142,4 @@ EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT= '587'
 EMAIL_USE_TLS=True
+

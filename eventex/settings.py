8051794104baf47f9ffc6ab227513c5a3640a920
gabriel <gabrielgamersbr@gmail.com>
2022-03-08 22:02:59 -0300

    formulario funcionando 100%

--- eventex/settings.py
+++ eventex/settings.py
@@ -128,14 +128,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #django_heroku.settings(locals())
 
-#email configuration
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST= config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast = int)
-EMAIL_USE_TLS = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT =  config('EMAIL_PORT')
+#email configuration nao funciona no .ENV nao sei o porque
+#EMAIL_BACKEND = config('EMAIL_BACKEND')
+#EMAIL_HOST= config('EMAIL_HOST')
+#EMAIL_PORT = config('EMAIL_PORT', cast = int)
+#EMAIL_USE_TLS = config('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+#EMAIL_PORT =  config('EMAIL_PORT')
 
+EMAIL_HOST_USER='seucarlinhos6@gmail.com'
+EMAIL_HOST_PASSWORD='seucarlinhos12@'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT='587'
+EMAIL_USE_TLS='True'
 
 
 

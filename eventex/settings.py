4af0321da8ae53ee9fbb6107c01c410d613fa47f
gabriel <gabrielgamersbr@gmail.com>
2022-03-08 21:50:55 -0300

    teste

--- eventex/settings.py
+++ eventex/settings.py
@@ -129,16 +129,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #django_heroku.settings(locals())
 
 #email configuration
-"""EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST= config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast = int)
 EMAIL_USE_TLS = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-"""
-EMAIL_HOST_USER='seucarlinhos6@gmail.com'
-EMAIL_HOST_PASSWORD='seucarlinhos12@'
-EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT= '587'
-EMAIL_USE_TLS=True
+EMAIL_PORT=  config(EMAIL_PORT)
+
+
+
 

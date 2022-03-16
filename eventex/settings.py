980db117ac6bbe8ccd09c62dec43e5d839cafbd2
Gabriel <gabrielgamersbr@gmail.com>
2022-03-15 21:59:56 -0300

    testes

--- eventex/settings.py
+++ eventex/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 DEFAULT_FROM_EMAIL = 'seucarlinhos6@gmail.com'
 
+DEBUG=True
+
 EMAIL_HOST_USER = 'seucarlinhos6@gmail.com'
 EMAIL_HOST_PASSWORD = 'vaitomarnocuhacker'
 # Application definition

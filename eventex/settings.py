fbd9ebc7b9ae029c6a96468542c37f73f2103750
Gabriel <gabrielgamersbr@gmail.com>
2022-03-15 21:44:02 -0300

    sS'

--- eventex/settings.py
+++ eventex/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+EMAIL_HOST_USER='seucarlinhos6@gmail.com'
+EMAIL_HOST_PASSWORD='vaitomarnocuhacker'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
